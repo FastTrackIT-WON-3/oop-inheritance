d88076c3b717e43c99606b2c3e23c3623b54971f
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-02-16 20:22:11 +0200

    Sample with a class hierarchy

---
+++ OopInheritance/OopInheritance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OopInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExcelFile paycheck = new ExcelFile("Paycheck", "Excel 2019");
+            // general functionality available for all files
+            paycheck.Open();
+            // specific functionality for Excel files
+            paycheck.PerformCalculation("A = B + C");
+
+            PdfFile book = new PdfFile("Book ABC", false);
+            // general functionality available for all files
+            book.Open();
+            // specific functionality for Pdf files
+            book.AttemptEdit();
+
+            Console.WriteLine("-----------------------------------");
+
+            OpenFiles(paycheck, book);
+        }
+
+        private static void OpenFiles(params File[] files)
+        {
+            foreach (File file in files)
+            {
+                file.Open();
+            }
+        }
+    }
+}
